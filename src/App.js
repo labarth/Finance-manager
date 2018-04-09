000feb510077f2f0a2902bff5bf91eb4f2cffa98
alexandr matiugin <alexander.matiugin@pandadoc.com>
2018-04-09 09:05:50 +0300

    move example action to mainpage.js

--- src/App.js
+++ src/App.js
@@ -1,59 +1,12 @@
 import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
 import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
-import styled from 'styled-components';
-import toggleTitleAction from './redux/actions/toggleTitle';
 import MainPage from './pages/MainPage';
 
-const Title = styled.h1`
-  font-size: 30px;
-  color: blue;
-`;
 
-const mapStateToProps = state => ({
-  title: state.toggleTitle,
-});
-
-const mapDispatchToProps = dispatch => ({
-  toggleTitle: data => dispatch(toggleTitleAction(data)),
-});
-
-@connect(mapStateToProps, mapDispatchToProps)
 class App extends PureComponent {
-  static propTypes = {
-    title: PropTypes.string,
-    toggleTitle: PropTypes.func.isRequired,
-  }
-
-  static defaultProps = {
-    title: 'undefined title',
-  }
-
-  state = {
-    active: false,
-  };
-
-  handleClick = () => {
-    const { toggleTitle } = this.props;
-
-    this.setState({
-      active: !this.state.active,
-    }, () => (
-      this.state.active ?
-        toggleTitle('deactivate title')
-        :
-        toggleTitle('activate title')));
-  }
-
   render() {
-    const { title } = this.props;
-
     return (
       <div>
-        <Title onClick={this.handleClick}>
-          {title}
-        </Title>
         <Router>
           <div>
             <Link to={`/main`}> Main page</Link>
