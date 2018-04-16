9ca472a523842ca04ac5f9dd9dd3368a3ad786f5
alexandr matiugin <alexander.matiugin@pandadoc.com>
2018-04-16 13:17:57 +0300

    remove artefact code

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,22 +1,11 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Button from 'components/Button';
 import Title from 'components/Title';
 import { signOutWithGoogle } from 'redux/reducer/reducers/authWithGoogle';
-import toggleTitleAction from 'redux/actions/toggleTitle';
 
 
-const mapStateToProps = state => ({
-  title: state.toggleTitle,
-});
-
-const mapDispatchToProps = dispatch => ({
-  toggleTitle: data => dispatch(toggleTitleAction(data)),
-});
-
-@connect(mapStateToProps, mapDispatchToProps)
 class MainPage extends Component {
   static propTypes = {
     user: PropTypes.object,
