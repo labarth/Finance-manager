da6dc6ffc8a84c7650f9322d7d49f5fd996aef3b
alexandr matiugin <alexander.matiugin@pandadoc.com>
2018-09-26 11:06:52 +0300

    refactoring auth

--- src/redux/reducer/reducers/categories.js
+++ src/redux/reducer/reducers/categories.js
@@ -2,12 +2,14 @@ import { Record, List, Map } from 'immutable';
 import { handleActions } from 'redux-actions';
 import { categoryActions } from 'redux/actions/categoryActions';
 
+const initialList = List([
+  Map({ label: 'Дом', value: 'Дом' }),
+  Map({ label: 'Еда', value: 'Еда' }),
+  Map({ label: 'Авто', value: 'Авто' }),
+]);
+
 const ItemsSchema = Record({
-  list: List([
-    Map({ label: 'Дом', value: 'Дом' }),
-    Map({ label: 'Еда', value: 'Еда' }),
-    Map({ label: 'Авто', value: 'Авто' }),
-  ]),
+  list: initialList,
   loading: false,
 });
 
@@ -20,7 +22,7 @@ const getDbCategoriesSuccess = (state = initialState, { payload }) => {
   if (payload.size) {
     return state.set('loading', false).set('list', payload);
   }
-  return state.set('loading', false);
+  return state.set('loading', false).set('list', initialList);
 };
 
 export const categories = handleActions({
